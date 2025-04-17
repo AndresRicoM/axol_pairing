4c61b4a76466a0a71d873138512a0abc6b424f3b
unknown <alejandro.hernandez4819@alumnos.udg.mx>
2025-04-16 18:24:16 -0600

    Add:[Tank] New no wifi protocol

--- sensors/tank_sensor/src/main.cpp
+++ sensors/tank_sensor/src/main.cpp
@@ -1,19 +1,19 @@
 /*
 
-   █████╗ ██╗  ██╗ ██████╗ ██╗
-  ██╔══██╗╚██╗██╔╝██╔═══██╗██║
-  ███████║ ╚███╔╝ ██║   ██║██║
-  ██╔══██║ ██╔██╗ ██║   ██║██║
-  ██║  ██║██╔╝ ██╗╚██████╔╝███████╗
-  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝
+   █████╗ ██╗  ██╗ ██████╗ ██╗     ████████╗ █████╗ ███╗  ██╗██╗  ██╗
+  ██╔══██╗╚██╗██╔╝██╔═══██╗██║     ╚══██╔══╝██╔══██╗████╗ ██║██║ ██╔╝
+  ███████║ ╚███╔╝ ██║   ██║██║        ██║   ███████║██╔██╗██║█████╔╝ 
+  ██╔══██║ ██╔██╗ ██║   ██║██║        ██║   ██╔══██║██║╚████║██╔═██╗ 
+  ██║  ██║██╔╝ ██╗╚██████╔╝███████╗   ██║   ██║  ██║██║ ╚███║██║  ██╗
+  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚══╝╚═╝  ╚═╝
 
   ᓬ(• - •)ᕒ
 
   Axol sensing system.
 
-   Code for tank quantity sensor. The seansor uses a vl53l4 optical sensor to detemine its distance from the water's surface.
-   The data is then used to calculate the quanity and fill percentage in the database. Volume is calaculated in the database backend.
-   You need to measure containers dimensions and capacity to register this device for the backend to calculate volume correctly.
+  Axol Tank - Uses a Time of Flight (TOF) sensor
+  to measure the distance between the water surface
+  and the top of the tank, calculating the water level.
 
    Andres Rico - aricom@mit.edu
 
@@ -31,7 +31,7 @@
 #include <stdint.h>
 #include <assert.h>
 #include <stdlib.h>
-
+//CONSTANTS
 #define DEV_I2C Wire
 #define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */
 #define TIME_TO_SLEEP 10       /* Time ESP32 will go to sleep (in seconds) */
@@ -43,7 +43,9 @@ void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len);
 void check_data();
 void check_pairing_connection();
 
+////FLAGS FOR SETUP WITH HOMEHUB AND NETWORK////////
 bool received_message = false;
+bool data_sent = false;
 
 // Receiver address
 uint8_t broadcastAddress[] = {255, 255, 255, 255, 255, 255}; // MAC Address for receiving homehub.
@@ -71,6 +73,18 @@ struct pairing_data pairingData = {};
 String address = WiFi.macAddress();
 char mac_add[50];
 
+void printMacAddress(const uint8_t *mac)
+{
+  Serial.print("[printMacAddress] Printing mac address: ");
+  for (int i = 0; i < 6; i++)
+  {
+    if (i > 0)
+      Serial.print(":");
+    Serial.print(mac[i], HEX);
+  }
+  Serial.println();
+}
+
 void handshake()
 {
   esp_err_t result = esp_now_send((const uint8_t *)broadcastAddress, (uint8_t *)&pairingData, sizeof(pairingData));
@@ -92,6 +106,21 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  data_sent = true;
+}
+
+String macToString(const uint8_t *mac)
+{
+  String macStr = "";
+  for (int i = 0; i < 6; i++)
+  {
+    if (i > 0)
+      macStr += ":";
+    macStr += String(mac[i], HEX);
+  }
+  macStr.toUpperCase(); // Ensure the MAC address is in uppercase
+
+  return macStr;
 }
 
 void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
@@ -127,19 +156,15 @@ void check_pairing_connection()
   String savedMAC = preferences.getString("mac", "");
   preferences.end();
 
-  if (savedMAC.length() > 0)
+  if (savedMAC.length() > 0 && savedMAC != "00:00:00:00:00:00")
   {
     stringToMacAddress(savedMAC, broadcastAddress);
 
-    Serial.println("Data loaded from EEPROM:");
-    Serial.print("BROADCAST MAC Address: ");
-    for (int i = 0; i < 6; i++)
-    {
-      if (i > 0)
-        Serial.print(":");
-      Serial.print(broadcastAddress[i], HEX);
-    }
-    Serial.println();
+    Serial.println("[check_pairing_connection] Data loaded from EEPROM:");
+    Serial.print("[check_pairing_connection] BROADCAST MAC Address: ");
+    printMacAddress(broadcastAddress);
+    Serial.print("[check_pairing_connection] SavedMAC string: ");
+    Serial.println(savedMAC);
     return;
   }
 
@@ -149,17 +174,18 @@ void check_pairing_connection()
   {
     delay(300);
   }
+  delay(200);
+  // Convert broadcastAddress to String
+  String macStr = macToString(broadcastAddress);
 
-  if (strlen(pairingData.mac_addr) > 0)
+  if (macStr.length() > 0)
   {
-    Serial.print("Homehub MAC Address: ");
-    Serial.println(pairingData.mac_addr);
+    Serial.println("[check_pairing_connection] Homehub MAC Address... ");
+    printMacAddress(broadcastAddress);
 
     preferences.begin("sensor-data", false);
-    preferences.putString("mac", String(pairingData.mac_addr));
+    preferences.putString("mac", macStr);
     preferences.end();
-
-    stringToMacAddress(pairingData.mac_addr, broadcastAddress);
   }
   else
   {
@@ -239,28 +265,54 @@ void setup()
   Serial.println(myData.height);
 
   address.toCharArray(mac_add, 50);
+  Serial.println("[setup] MAC Address for this device:");
   Serial.println(mac_add);
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
-  esp_now_init();
+
 
   // WiFi.printDiag(Serial); // Uncomment to verify channel number before
   esp_wifi_set_promiscuous(true);
   esp_wifi_set_channel(wifi_channel, WIFI_SECOND_CHAN_NONE);
   esp_wifi_set_promiscuous(false);
+  // Forcing channel synchronization
+  delay(100);
   // WiFi.printDiag(Serial); // Uncomment to verify channel change after
+  Serial.println("[setup] Checking pairing connection...");
+  check_pairing_connection();
+  delay(100);
+  Serial.println("[setup] WiFi Info...");
+  WiFi.printDiag(Serial); // Uncomment to verify channel change after
+
+  // Init ESP-NOW
+  // get the status of Trasnmitted packet
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("{setup] Error init ESP-NOW");
+    return;
+  }
 
   Serial.println("Registering callbacks...");
   esp_now_register_send_cb(OnDataSent);
   esp_now_register_recv_cb(OnDataRecv);
 
-  Serial.println("Checking pairing connection...");
-  check_pairing_connection();
+  Serial.println("///////////////////////");
+  Serial.println("[setup] BROADCAST ADDRESS FROM SETUP...");
+  printMacAddress(broadcastAddress);
+  Serial.println("///////////////////////");
+
   send_espnow();
+
+  delay(500);
+
 
   /////////////////Change value for higher or lower frequency of data collection. This is the time the ESP32 will sleep for.
   esp_sleep_enable_timer_wakeup(3600000000 * 12); // TIME_TO_SLEEP * uS_TO_S_FACTOR); //Twice per day. Value is in microseconds.
 
+  // Cleaning before going to sleep
+  esp_now_deinit();
+  WiFi.mode(WIFI_OFF);
   esp_wifi_stop();
 
   esp_deep_sleep_start();
@@ -268,22 +320,36 @@ void setup()
 
 void send_espnow()
 {
-  address.toCharArray(mac_add, 50);
-  Serial.println(mac_add);
-
-  Serial.println("Wifi channel is:");
-  Serial.println(wifi_channel);
-
-  esp_wifi_set_channel(wifi_channel, WIFI_SECOND_CHAN_NONE);
+  Serial.println("***************");
+  Serial.println("[send_espnow] Broadcast address before peer...");
+  printMacAddress(broadcastAddress);
+  Serial.println("***************");
 
   Serial.println("Copying data to struct...");
   strcpy(myData.id, mac_add);
   myData.type = 2; // Id 2 = Tank Level sensor.
 
+  address.toCharArray(mac_add, 50);
+  Serial.println(mac_add);
+
   Serial.println("Registering peer...");
   esp_now_peer_info_t peerInfo = {};
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = wifi_channel; // Set the channel to the same as the sender
+  peerInfo.ifidx = WIFI_IF_STA;    // Station Interface
   peerInfo.encrypt = false;
+
+  Serial.println("----------");
+  Serial.println("[send_espnow] Peer address:");
+  printMacAddress(peerInfo.peer_addr);
+  Serial.println("----------");
+  Serial.print("[send_espnow] Peer channel:");
+  Serial.println(peerInfo.channel);
+  Serial.print("[send_espnow] Peer encrypt:");
+  Serial.println(peerInfo.encrypt);
+  Serial.print("[send_espnow] Peer ifidx:");
+  Serial.println(peerInfo.ifidx);
+  Serial.println("[send_espnow] Peer info registered.");
 
   esp_err_t addPeerResult = esp_now_add_peer(&peerInfo);
   if (addPeerResult != ESP_OK)
@@ -292,15 +358,35 @@ void send_espnow()
     Serial.println(addPeerResult);
   }
 
-  Serial.println("Sending data via ESP-NOW...");
+  // Send message via ESP-NOW
+  Serial.println("[send_espnow] Sending data via ESP-NOW...");
+  Serial.println("----------");
+  Serial.println("[send_espnow] Sending to... ");
+  printMacAddress(broadcastAddress);
+  Serial.println("----------");
+
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+
+  // Espera confirmación o timeout
+  unsigned long start = millis();
+  while (!data_sent && millis() - start < 200)
+  {
+    delay(10);
+  }
+
+  if (!data_sent)
+  {
+    Serial.println("[send_espnow] No confirmación de envío");
+  }
+
   if (result != ESP_OK)
   {
-    Serial.print("Failed to send data, error code: ");
+    Serial.print("[send_espnow] Failed to send data, error code: ");
     Serial.println(result);
   }
 }
 
 void loop()
 {
+  
 }
